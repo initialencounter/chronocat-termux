b8d1e75e3391d7ec9594d7a7b78389b215f24a07
initialencounter <2911583893@qq.com>
2024-01-24 23:19:37 +0800

    feat: upgrade chronocat-docker

---
+++ lite.sh
@@ -0,0 +1,74 @@
+# 使用基于Ubuntu 22.04的基础映像
+FROM ubuntu:22.04
+
+# 设置环境变量
+ENV DEBIAN_FRONTEND=noninteractive
+ENV VNC_PASSWD=vncpasswd
+
+# 安装必要的软件包
+apt-get update && apt-get install -y \
+    openbox \
+    curl \
+    unzip \
+    x11vnc \
+    xvfb \
+    fluxbox \
+    supervisor \
+    libnotify4 \
+    libnss3 \
+    xdg-utils \
+    libsecret-1-0 \
+    libgbm1 \
+    libasound2 \
+    fonts-wqy-zenhei \
+    git \
+    gnutls-bin \    
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# 安装novnc
+git config --global http.sslVerify false && git config --global http.postBuffer 1048576000
+cd /opt && git clone https://github.com/novnc/noVNC.git
+cd opt/noVNC/utils && git clone https://github.com/novnc/websockify.git
+cp /opt/noVNC/vnc.html /opt/noVNC/index.html     
+
+# 安装Linux QQ
+curl -o /root/linuxqq_3.2.5-20979_arm64.deb https://dldir1.qq.com/qqfile/qq/QQNT/c64ca459/linuxqq_3.2.5-20979_arm64.deb
+dpkg -i /root/linuxqq_3.2.5-20979_arm64.deb && apt-get -f install -y && rm /root/linuxqq_3.2.5-20979_arm64.deb
+
+# 安装LiteLoader
+curl -L -o /tmp/LiteLoaderQQNT.zip https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/download/1.0.2/LiteLoaderQQNT.zip \
+    && unzip /tmp/LiteLoaderQQNT.zip -d /opt/QQ/resources/app/ \
+    && rm /tmp/LiteLoaderQQNT.zip
+# 修改/opt/QQ/resources/app/package.json文件
+sed -i 's/"main": ".\/app_launcher\/index.js"/"main": ".\/LiteLoader"/' /opt/QQ/resources/app/package.json
+
+# 安装chronocat  
+cp chronocat-v0.0.71.zip /tmp/chronocat-llqqnt.zip \
+  && mkdir -p /root/LiteLoaderQQNT/plugins \
+  && unzip /tmp/chronocat-llqqnt.zip -d /root/LiteLoaderQQNT/plugins/ \
+  && rm /tmp/chronocat-llqqnt.zip
+
+# 创建必要的目录
+mkdir -p ~/.vnc
+
+# 创建启动脚本
+echo "#!/bin/bash" > ~/start.sh
+echo "rm /tmp/.X1-lock" >> ~/start.sh
+echo "Xvfb :1 -screen 0 1280x1024x16 &" >> ~/start.sh
+echo "export DISPLAY=:1" >> ~/start.sh
+echo "fluxbox &" >> ~/start.sh
+echo "x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &" >> ~/start.sh
+echo "nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &" >> ~/start.sh
+echo "x11vnc -storepasswd \$VNC_PASSWD ~/.vnc/passwd" >> ~/start.sh
+echo "su -c 'qq' root" >> ~/start.sh
+chmod +x ~/start.sh
+
+# 配置supervisor
+echo "[supervisord]" > /etc/supervisor/supervisord.conf
+echo "nodaemon=true" >> /etc/supervisor/supervisord.conf
+echo "[program:x11vnc]" >> /etc/supervisor/supervisord.conf
+echo "command=/usr/bin/x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd" >> /etc/supervisor/supervisord.conf
+
+# 设置容器启动时运行的命令
+CMD ["/bin/bash", "-c", "/root/start.sh"]
