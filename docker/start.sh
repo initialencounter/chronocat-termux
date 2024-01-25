31df3cf1b0bc511c3bb3e2e11799354da3874e01
initialencounter <2911583893@qq.com>
2024-01-25 22:06:48 +0800

    fix: start,sh qq

--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,5 @@ fluxbox &
 x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
 nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
 x11vnc -storepasswd VNC_PASSWD ~/.vnc/passwd
-su -c 'qq' root"
+su -c 'qq' root
 chmod +x ~/start.sh
