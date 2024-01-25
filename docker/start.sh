5044d48442e3ea741000a88067d444a7139c1eff
initialencounter <2911583893@qq.com>
2024-01-26 00:56:03 +0800

    feat: available but manual

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-rm /tmp/.X1-lock
-Xvfb :1 -screen 0 1280x1024x16 &
+service dbus start
+rm -f /tmp/.X1-lock
 export DISPLAY=:1
+Xvfb :1 -screen 0 720x512x16 &
 fluxbox &
+sleep 2
 x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
+sleep 2
 nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
-x11vnc -storepasswd VNC_PASSWD ~/.vnc/passwd
-su -c 'qq' root
-chmod +x ~/start.sh
+sleep 2
+x11vnc -storepasswd vncpasswd ~/.vnc/passwd
+sleep 2
+qq --no-sandbox --chrono-mode=headless3
