02bba0102fc32a44f0e2cc06d76cdbdcc77a8415
initialencounter <2911583893@qq.com>
2024-01-26 23:20:31 +0800

    feat: update termux scripts

---
+++ cc.sh
@@ -0,0 +1,73 @@
+# 设置环境变量
+export DEBIAN_FRONTEND=noninteractive
+export VNC_PASSWD=vncpasswd
+export DEBIAN_FRONTEND=noninteractive
+
+# 安装必要的软件包
+apt-get update && apt-get install -y \
+    openbox \
+    curl \
+    unzip \
+    x11vnc \
+    xvfb \
+    fluxbox \
+    supervisor \
+    libnotify4 \
+    libnss3 \
+    xdg-utils \
+    libsecret-1-0 \
+    libgbm1 \
+    libasound2 \
+    fonts-wqy-zenhei \
+    git \
+    gnutls-bin
+
+# 安装novnc
+git config --global http.sslVerify false && \
+    git config --global http.postBuffer 1048576000 && \
+    cd /opt && git clone https://mirror.ghproxy.com/https://github.com/novnc/noVNC.git && \
+    cd /opt/noVNC/utils && git clone https://mirror.ghproxy.com/https://github.com/novnc/websockify.git && \
+    cp /opt/noVNC/vnc.html /opt/noVNC/index.html     
+
+# 安装Linux QQ
+curl -o /root/linuxqq_3.2.5-20979_amd64.deb https://dldir1.qq.com/qqfile/qq/QQNT/c64ca459/linuxqq_3.2.5-20979_amd64.deb && \
+    dpkg -i /root/linuxqq_3.2.5-20979_amd64.deb && apt-get -f install -y && rm /root/linuxqq_3.2.5-20979_amd64.deb
+
+# 安装LiteLoader
+curl -L -o /tmp/LiteLoaderQQNT.zip https://mirror.ghproxy.com/https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/download/1.0.2/LiteLoaderQQNT.zip && \
+    mkdir -p /opt/QQ/resources/app/LiteLoader && \
+    unzip /tmp/LiteLoaderQQNT.zip -d /opt/QQ/resources/app/LiteLoader && \
+    sed -i '1s/^/require("\/opt\/QQ\/resources\/app\/LiteLoader");\n/' /opt/QQ/resources/app/app_launcher/index.js && \
+    rm /tmp/LiteLoaderQQNT.zip
+
+# 安装chronocat  
+cp chronocat-llqqntv1-v0.0.71.zip /tmp/chronocat-llqqntv1-v0.0.71.zip
+mkdir -p /opt/QQ/resources/app/LiteLoader/plugins && \
+  unzip /tmp/chronocat-llqqntv1-v0.0.71.zip -d /opt/QQ/resources/app/LiteLoader/plugins/ && \
+  rm /tmp/chronocat-llqqntv1-v0.0.71.zip
+
+# 创建必要的目录
+mkdir -p ~/.vnc
+
+# 创建启动脚本
+# 配置supervisor
+echo "[supervisord]" > /etc/supervisor/supervisord.conf && \
+    echo "nodaemon=true" >> /etc/supervisor/supervisord.conf && \
+    echo "[program:x11vnc]" >> /etc/supervisor/supervisord.conf && \
+    echo "command=/usr/bin/x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd" >> /etc/supervisor/supervisord.conf
+
+echo "#!/bin/bash
+service dbus start
+rm -f /tmp/.X1-lock
+export DISPLAY=:1
+Xvfb :1 -screen 0 720x512x16 &
+fluxbox &
+sleep 2
+x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
+sleep 2
+nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
+sleep 2
+x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd
+sleep 2
+qq --no-sandbox
+" >> ~/.bashrc
--- onekey.sh
+++ onekey.sh
@@ -104,8 +104,10 @@ echo "授予启动脚本执行权限"
 chmod +x $sys_name-$AH.sh
 echo -e "现在可以执行 ./$sys_name-$AH.sh 运行 $sys_name-$AH系统"
 
-# curl -O https://gitee.com/initencunter/chronocat-termux/raw/main/install_chronocat.sh
-# mv install_chronocat.sh $sys_name-$AH/root/start.sh
-# echo "bash start.sh" >> $sys_name-$AH/root/.bashrc
-# echo "bash $sys_name-$AH.sh" > .bashrc
-# bash ./$sys_name-$AH.sh·
+
+curl -O https://gitee.com/initencunter/chronocat-termux/raw/main/cc.sh
+mv cc.sh $sys_name-$AH/root/cc.sh
+
+unset LD_PRELOAD
+proot --bind=/vendor --bind=/system --bind=/data/data/com.termux/files/usr --bind=/storage --bind=/storage/self/primary:/sdcard --bind=/data/data/com.termux/files/home --bind=/data/data/com.termux/cache --bind=/data/dalvik-cache --bind=$sys_name-$AH/tmp:/dev/shm --bind=$sys_name-$AH/etc/proc/vmstat:/proc/vmstat --bind=$sys_name-$AH/etc/proc/version:/proc/version --bind=$sys_name-$AH/etc/proc/uptime:/proc/uptime --bind=$sys_name-$AH/etc/proc/stat:/proc/stat --bind=$sys_name-$AH/etc/proc/loadavg:/proc/loadavg  --bind=$sys_name-$AH/etc/proc/bus/pci/00:/proc/bus/pci/00 --bind=$sys_name-$AH/etc/proc/devices:/proc/bus/devices --bind=$sys_name-$AH/etc/proc/bus/input/devices:/proc/bus/input/devices --bind=$sys_name-$AH/etc/proc/modules:/proc/modules   --bind=/sys --bind=/proc/self/fd/2:/dev/stderr --bind=/proc/self/fd/1:/dev/stdout --bind=/proc/self/fd/0:/dev/stdin --bind=/proc/self/fd:/dev/fd --bind=/proc --bind=/dev/urandom:/dev/random --bind=/data/data/com.termux/files/usr/tmp:/tmp --bind=/data/data/com.termux/files:$sys_name-$AH/termux --bind=/dev --root-id --cwd=/root -L --kernel-release=5.17.18-perf --sysvipc --link2symlink --kill-on-exit --rootfs=$sys_name-$AH/ /usr/bin/env -i HOME=/root LANG=zh_CN.UTF-8 TERM=xterm-256color /bin/su -l root bash /root/cc.sh
+
