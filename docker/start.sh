fa49410e56f3e45bb51c803507cdf6c906026d63
initialencounter <2911583893@qq.com>
2024-01-31 00:13:43 +0800

    fix: passwd set fail

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,6 @@ x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/pass
 sleep 2
 nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
 sleep 2
-x11vnc -storepasswd vncpasswd ~/.vnc/passwd
+x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd
 sleep 2
 qq --no-sandbox
