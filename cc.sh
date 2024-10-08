69fd3d119793c89a30662b13f4a8a5d2d36ab4bc
initialencounter <2911583893@qq.com>
2024-10-08 20:19:21 +0800

    cc: v0.2.16 -> v0.2.19

--- cc.sh
+++ cc.sh
@@ -58,14 +58,17 @@ export DISPLAY=:1
 qq --no-sandbox --disable-gpu" > /root/start.sh
 
 # 安装chronocat  
-version=v0.2.16
+version=v0.2.19
 curl -L -o /tmp/chronocat-llqqnt-engine-chronocat-api.zip https://github.com/chrononeko/chronocat/releases/download/$version/chronocat-llqqnt-engine-chronocat-api-$version.zip
 curl -L -o /tmp/chronocat-llqqnt-engine-chronocat-event.zip https://github.com/chrononeko/chronocat/releases/download/$version/chronocat-llqqnt-engine-chronocat-event-$version.zip
+curl -L -o /tmp/chronocat-llqqnt-engine-media.zip https://github.com/chrononeko/chronocat/releases/download/$version/chronocat-llqqnt-engine-media-$version.zip
+curl -L -o /tmp/chronocat-llqqnt-engine-crychiccat.zip https://github.com/chrononeko/chronocat/releases/download/$version/chronocat-llqqnt-engine-crychiccat-$version.zip
 curl -L -o /tmp/chronocat-llqqnt.zip https://github.com/chrononeko/chronocat/releases/download/$version/chronocat-llqqnt-$version.zip
 unzip /tmp/chronocat-llqqnt-engine-chronocat-api.zip  -d /LiteLoader/plugins/
 unzip /tmp/chronocat-llqqnt-engine-chronocat-event.zip  -d /LiteLoader/plugins/
 unzip /tmp/chronocat-llqqnt.zip -d /LiteLoader/plugins/
-
+unzip /tmp/chronocat-llqqnt-engine-media.zip -d /LiteLoader/plugins/
+unzip /tmp/chronocat-llqqnt-engine-crychiccat.zip -d /LiteLoader/plugins/
 
 echo -e "chronocat 安装完成
 现在你可以输入命令 \e[32mbash debian-sid-arm64.sh\e[0m 进入容器
